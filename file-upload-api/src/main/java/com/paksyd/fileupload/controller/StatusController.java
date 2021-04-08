58b5efb8170bade413b45efbbce6367576244baa
Dávid Paksy <paksydavid@gmail.com>
2021-04-08 18:48:37 +0200

    Backend: Added CORS config
    to allow requests from http://localhost:4200

--- file-upload-api/src/main/java/com/paksyd/fileupload/controller/StatusController.java
+++ file-upload-api/src/main/java/com/paksyd/fileupload/controller/StatusController.java
@@ -7,10 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -26,6 +23,7 @@ public class StatusController {
         this.uploadService = uploadService;
     }
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @PostMapping(path = "{id}")
     public ResponseEntity<StatusResponse> getStatus(@PathVariable("id") UUID id) {
         // TODO: Check if id can be null here and return 400 if it is!
